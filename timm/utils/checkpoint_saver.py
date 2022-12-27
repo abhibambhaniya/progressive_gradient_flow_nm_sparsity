cc5cba575d71373f80f644d84fe02073ca6a9bfd
Amir Yazdanbakhsh <ayazdan@google.com>
2022-12-26 16:15:09 -0800

    remove link because gcs does not support it

--- timm/utils/checkpoint_saver.py
+++ timm/utils/checkpoint_saver.py
@@ -9,6 +9,7 @@ import glob
 import operator
 import os
 import logging
+import shutil
 
 import torch
 
@@ -66,7 +67,8 @@ class CheckpointSaver:
         last_save_path = os.path.join(self.checkpoint_dir, 'last' + self.extension)
         self._save(tmp_save_path, epoch, metric)
         if os.path.exists(last_save_path):
-            os.unlink(last_save_path)  # required for Windows support.
+            # os.unlink(last_save_path)  # required for Windows support.
+            os.remove(last_save_path)
         os.rename(tmp_save_path, last_save_path)
         worst_file = self.checkpoint_files[-1] if self.checkpoint_files else None
         if (len(self.checkpoint_files) < self.max_history
@@ -75,7 +77,8 @@ class CheckpointSaver:
                 self._cleanup_checkpoints(1)
             filename = '-'.join([self.save_prefix, str(epoch)]) + self.extension
             save_path = os.path.join(self.checkpoint_dir, filename)
-            os.link(last_save_path, save_path)
+            # os.link(last_save_path, save_path)
+            os.copyfile(last_save_path, save_path)
             self.checkpoint_files.append((save_path, metric))
             self.checkpoint_files = sorted(
                 self.checkpoint_files, key=lambda x: x[1],
@@ -91,8 +94,10 @@ class CheckpointSaver:
                 self.best_metric = metric
                 best_save_path = os.path.join(self.checkpoint_dir, 'model_best' + self.extension)
                 if os.path.exists(best_save_path):
-                    os.unlink(best_save_path)
-                os.link(last_save_path, best_save_path)
+                    # os.unlink(best_save_path)
+                    os.remove(best_save_path)
+                # os.link(last_save_path, best_save_path)
+                shutil.copyfile(last_save_path, best_save_path)
 
         return (None, None) if self.best_metric is None else (self.best_metric, self.best_epoch)
 
